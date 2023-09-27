86d7a0524f0532dfad9f6d6261207f3ea96ea762
BlogyBOY <121603305+BlogyBOY@users.noreply.github.com>
2023-09-28 00:49:58 +0530

    Commit 1
    Commit 1

--- token-based-auth-core-react/ecom-app/src/App.js
+++ token-based-auth-core-react/ecom-app/src/App.js
@@ -1,25 +1,73 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route } from 'react-router';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+
+export default class App extends Component {
+  static displayName = App.name;
+
+
+  render() {
+    return (
+
+      <div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      </div>
 
-export default App;
+      //       //if(SessionManager.getToken())
+      //       // SessionManager.getToken() ? (
+      //         // <Layout>
+      //         //  {/*  <Route exact path='/home' component={Home} />
+
+      //         //   <Route exact path='/home' component={Home} />
+
+      //         //   <Route path='/logout' component={Logout} />
+      //         //   <Route path='/registration' component={Registration} />
+
+
+      //         //   <Route path='/banking/customers' component={Customers} />
+      //         //   <Route path='/banking/customer/create' component={Create} />
+      //         //   <Route path='/banking/customer/edit/:id' component={Edit}></Route>
+      //         //   <Route path='/banking/customer/delete/:id' component={Delete}></Route>
+
+
+
+      //         //   <Route path='/counter' component={Counter} />
+      //         //   <Route path='/fetch-data' component={FetchData} />
+
+      //         //   <Route path='/admin/users' component={Users}></Route>
+      //         //   <Route path='/admin/user/edit/:id' component={UpdateUser}></Route>
+      //         //   <Route path='/admin/user/delete/:id' component={DeleteUser}></Route>
+
+      //         //   <Route path='/admin/roles' component={Roles}></Route>
+      //         //   <Route path='/admin/role/create' component={CreateRole}></Route>
+      //         //   <Route path='/admin/role/edit/:id' component={EditRole}></Route>
+      //         //   <Route path='/admin/role/delete/:id' component={DeleteRole}></Route>
+
+      //         //   <Route path='/admin/usersroles' component={UsersRole}></Route>
+      //         //   <Route path='/admin/user/create' component={CreateUser}></Route>
+
+      //         // */}
+
+      //         // </Layout>
+
+      //       // ) : (
+
+      //       //     <>
+      //       //       <Layout>
+      //       //         <Route path={'/', "/login"} component={Login} />
+      //       //       <Route path='/admin/user/register' component={CreateUser}></Route>
+      //       //       </Layout>
+      //       //     </>
+
+      //          //)
+      //   //  );
+      //   //}
+
+    );
+  }
+}
