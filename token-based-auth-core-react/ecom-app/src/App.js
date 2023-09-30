23ece9e51501a4192a5616a02c30698684c9de8f
BlogyBOY <121603305+BlogyBOY@users.noreply.github.com>
2023-09-30 21:11:21 +0530

    Update App.js

--- token-based-auth-core-react/ecom-app/src/App.js
+++ token-based-auth-core-react/ecom-app/src/App.js
@@ -25,134 +25,18 @@ export default class App extends Component {
   static displayName = App.name;
 
   //export default function App() {
-    render() { return (
-
-      return ( 
-        <div className="App"> 
-          <Routes> 
-          <Route path="/" element={<Layout />}>
-             <Route index element={<Home />} />
-             <Route path="./login" element={<Login />} />
-             <Route path="./home" element={<Home />} />
-             <Route path='/logout' element={Logout} />
-             <Route path='/registration' element={Registration} />
-          </Routes> 
-        </div> 
-  // // <BrowserRouter basename={baseUrl}>
-  // //       <Routes>
-  // //         <Route path="/" element={<Layout />}>
-  // //           <Route index element={<Home />} />
-  // //           <Route path="./login" element={<Login />} />
-  // //           <Route path="./home" element={<Home />} />
-  // //           <Route path='/logout' element={Logout} />
-  // //           <Route path='/registration' element={Registration} />
-  // //           {/* <Route path="contact" element={<Contact />} /> */}
-  // //           {/* <Route path="*" element={<NoPage />} /> */}
-  // //         </Route>
-  // //       </Routes>
-  // //     </BrowserRouter>
-    )
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route index element={<Home />} />
+          <Route path="./login" element={<Login />} />
+          <Route path="./home" element={<Home />} />
+          <Route path='/logout' element={Logout} />
+          <Route path='/registration' element={Registration} />
+        </Routes>
+      </div>
+    );
   }
 }
-
-/** */
-
-
-// // // // render() {
-// // // //     return (
-
-// // // //       // <div>
-// // // //       //   <img src={logo} className="App-logo" alt="logo" />
-// // // //       //   <p>
-// // // //       //     Edit <code>src/App.js</code> and save to reload.
-// // // //       //   </p>
-// // // //       // </div>
-
-// // // //       // if (SessionManager.getToken()) {
-// // // //       SessionManager.getToken() ? (
-// // // //         <Layout>
-// // // //           {/* <Route element={<div style={{ background: 'red' }} > <Outlet /> */}
-// // // //           <h1>this is App.JS</h1>
-
-
-// // // //           /*// {
-// // // //             //Example for Outlet to act in place of Layout
-// // // //             // <Routes>
-// // // //             //     <Route path="/" element={<p>Home</p>} />
-// // // //             //     <Route element={<div style={{ background: 'red' }} > <Outlet /> </div>}>
-// // // //             //         <Route path="/login" element={<p>Login</p>} />
-// // // //             //     </Route>
-// // // //             // </Routes>
-
-
-// // // //             // <BrowserRouter>
-// // // //             //   <Routes>
-
-// // // //           }*/
-// // // //           <Route exact path='/home' element={Home} />
-
-// // // //           <Route path='/logout' element={Logout} />
-// // // //           <Route path='/registration' element={Registration} />
-
-
-// // // //           {/* <Route path='/banking/customers' element={Customers} />
-// // // //                 <Route path='/banking/customer/create' element={Create} />
-// // // //                 <Route path='/banking/customer/edit/:id' element={Edit}></Route>
-// // // //                 <Route path='/banking/customer/delete/:id' element={Delete}></Route>
-
-// // // //                 <Route path='/counter' element={Counter} />
-// // // //                 <Route path='/fetch-data' element={FetchData} />
-
-// // // //                 <Route path='/admin/users' element={Users}></Route>
-// // // //                 <Route path='/admin/user/edit/:id' element={UpdateUser}></Route>
-// // // //                 <Route path='/admin/user/delete/:id' element={DeleteUser}></Route>
-
-// // // //                 <Route path='/admin/roles' element={Roles}></Route>
-// // // //                 <Route path='/admin/role/create' element={CreateRole}></Route>
-// // // //                 <Route path='/admin/role/edit/:id' element={EditRole}></Route>
-// // // //                 <Route path='/admin/role/delete/:id' element={DeleteRole}></Route> */}
-
-// // // //           {/* <Route path='/admin/usersroles' element={UsersRole}></Route>
-// // // //           <Route path='/admin/user/create' element={CreateUser}></Route> */}
-// // // //           {/* </Routes>
-// // // //             </BrowserRouter> */}
-
-// // // //           {/* </ div>}>
-// // // //         </Route > */}
-// // // //         </Layout>
-// // // //         //</Route>
-
-// // // //       ) : (
-
-// // // //         <>
-// // // //           <Layout>
-// // // //             <h1>ELSE this is App.JS</h1>
-// // // //             {/* <Route> */}
-// // // //             {/* <Route path={'/', "/login"} element={Login} /> */}
-
-// // // //             {/* <Route path='/admin/user/register' element={CreateUser}></Route> */}
-// // // //             {/* <Routes>
-// // // //               <Route exact path='/home' element={Home} />
-// // // //             </Routes> */}
-
-// // // //             {/* <BrowserRouter>
-// // // //               <Routes> */}
-// // // //                 {/* <Route path={'/', "/login"} element={Login} /> */}
-
-// // // //                 <Route exact path="/" element={<Login />} />
-// // // //                 <Route exact path="/" element={<Home />} />
-// // // //                 {/* <Route path="/quiz" element={<Quiz />} /> */}
-
-// // // //               {/* </Routes>
-// // // //             </BrowserRouter> */}
-
-// // // //             {/* </Route> */}
-// // // //           </Layout>
-// // // //         </>
-
-// // // //       )
-// // // //     );
-// // // //   }
-
-  
-////}
