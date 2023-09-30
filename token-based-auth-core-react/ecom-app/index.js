2485844e017518e21735d82736986acb7be577e4
BlogyBOY <121603305+BlogyBOY@users.noreply.github.com>
2023-09-30 21:06:02 +0530

    Fix 01

--- token-based-auth-core-react/ecom-app/index.js
+++ token-based-auth-core-react/ecom-app/index.js
@@ -10,15 +10,27 @@ const baseUrl = '/';
 //const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
-https://www.appsloveworld.com/reactjs/100/2/error-error-a-route-is-only-ever-to-be-used-as-the-child-of-routes-element'
+//https://www.appsloveworld.com/reactjs/100/2/error-error-a-route-is-only-ever-to-be-used-as-the-child-of-routes-element'
 
-render(
-  <BrowserRouter basename={baseUrl}>
-    <Routes>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//sroot.render(<App />);
+
+
+ReactDOM.render(
+  <StrictMode>
+    <BrowserRouter>
       <App />
-    </Routes>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </StrictMode>,
   rootElement);
+
+// render(
+//   <BrowserRouter basename={baseUrl}>
+//     <Routes>
+//       <App />
+//     </Routes>
+//   </BrowserRouter>,
+//   rootElement);
 
 // ReactDOM.render(
 //   <BrowserRouter basename={baseUrl}>
--- token-based-auth-core-react/ecom-app/src/components/Auth/Login.js
+++ token-based-auth-core-react/ecom-app/src/components/Auth/Login.js
@@ -85,7 +85,7 @@ export default class Login extends Component {
         });
     }
 
-    registration(){
+    registration() {
         window.location.href = "/admin/user/register";
 
     }
@@ -97,56 +97,56 @@ export default class Login extends Component {
         }
 
         return (
-            <div className="row" style={{textAlign: "center"}}>
-            <div className="login-box col-md-4">
-                <div className="login-logo">
-                    <a href="/"><b>ECommerce</b></a>
-                </div>
-                <div className="login-box-body">
-                    <p className="login-box-msg">Sign in to access the application</p>
+            <div className="row" style={{ textAlign: "center" }}>
+                <div className="login-box col-md-4">
+                    <div className="login-logo">
+                        <a href="/"><b>ECommerce</b></a>
+                    </div>
+                    <div className="login-box-body">
+                        <p className="login-box-msg">Sign in to access the application</p>
 
-                    <div className="form-group has-feedback">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Please Enter Username"
-                            name="userName"
-                            onChange={this.onChange}
-                            onKeyDown={this.onKeyDown}
-                        />
-                        <span className="glyphicon glyphicon-user form-control-feedback" />
-                    </div>
-                    <div className="form-group has-feedback">
-                        <input type="password" className="form-control" placeholder="Please Enter Password" name="password"
-                            onChange={this.onChange} onKeyDown={this.onKeyDown}
-                        />
-                        <span className="glyphicon glyphicon-lock form-control-feedback" />
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4">
-                            <button className="btn btn-primary btn-block" onClick={this.login}>
-                                Sign In
-                            </button>
+                        <div className="form-group has-feedback">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Please Enter Username"
+                                name="userName"
+                                onChange={this.onChange}
+                                onKeyDown={this.onKeyDown}
+                            />
+                            <span className="glyphicon glyphicon-user form-control-feedback" />
                         </div>
-                        <div className="col-md-6">
-                            <button className="btn btn-primary btn-block" onClick={this.registration}>
-                                Create an account
-                            </button>
+                        <div className="form-group has-feedback">
+                            <input type="password" className="form-control" placeholder="Please Enter Password" name="password"
+                                onChange={this.onChange} onKeyDown={this.onKeyDown}
+                            />
+                            <span className="glyphicon glyphicon-lock form-control-feedback" />
                         </div>
-                        <div className="col-md-2">
-                            {content}
+                        <div className="row">
+                            <div className="col-md-4">
+                                <button className="btn btn-primary btn-block" onClick={this.login}>
+                                    Sign In
+                                </button>
+                            </div>
+                            <div className="col-md-6">
+                                <button className="btn btn-primary btn-block" onClick={this.registration}>
+                                    Create an account
+                                </button>
+                            </div>
+                            <div className="col-md-2">
+                                {content}
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-8" style={{ textAlign: "center", paddingTop: "16px" }}>
-                            <strong className="has-error" style={{ color: "red" }}>{this.state.errorMsg}</strong>
-                        </div>
-                        <div className="col-md-4">
-                            <ToastContainer></ToastContainer>
+                        <div className="row">
+                            <div className="col-md-8" style={{ textAlign: "center", paddingTop: "16px" }}>
+                                <strong className="has-error" style={{ color: "red" }}>{this.state.errorMsg}</strong>
+                            </div>
+                            <div className="col-md-4">
+                                <ToastContainer></ToastContainer>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
             </div>
         );
